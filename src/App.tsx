4946fc42e677fc6c0be720f8fe6914bc96efd316
Liza Krasnoshchok <nlight1122@gmail.com>
2021-07-23 17:40:28 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './app.scss';
+
+function App() {
+  return (
+    <div className="app">
+    </div>
+  );
+}
+
+export default App;
